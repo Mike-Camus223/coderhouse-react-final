a8bd7176579b25ed724d4974acead0af2eef57cb
michael <michaelcamposvilgmail.com>
2026-03-08 19:43:19 -0300

    new footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
 
         {/* LOGO */}
         <div>
-          <h2 className="text-4xl font-bold tracking-widest scale-y-125">
-            CAFECITO
+          <h2 className="text-3xl font-bold tracking-widest">
+            Cafecito
           </h2>
         </div>
 
@@ -91,25 +91,25 @@ const Footer = () => {
               Sí, quiero recibir descuentos y novedades.
             </label>
 
-            <button className="bg-amber-600 hover:bg-amber-700 text-black font-semibold px-6 py-2 text-sm mt-2">
+            <button className="bg-amber-600 hover:bg-amber-700 text-white font-semibold px-6 py-2 text-sm mt-2">
               SUSCRIBIRME
             </button>
           </div>
 
           {/* REDES */}
           <div className="mt-8">
-            <p className="text-amber-500 text-sm tracking-widest mb-4">
+            <p className="text-amber-500 text-sm text-center tracking-widest mb-4">
               CONECTATE CON NOSOTROS
             </p>
 
-            <div className="flex gap-4">
-              <div className="w-9 h-9 rounded-full bg-gray-700 flex items-center justify-center">
-                f
-              </div>
+            <div className="flex justify-center gap-4">
+              <Link to="https://www.facebook.com/" className="w-9 h-9 rounded-full bg-amber-500 flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook-icon lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
+              </Link>
 
-              <div className="w-9 h-9 rounded-full bg-gray-700 flex items-center justify-center">
-                ig
-              </div>
+              <Link to="https://www.instagram.com/" className="w-9 h-9 rounded-full bg-amber-500 flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram-icon lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
+              </Link>
             </div>
           </div>
         </div>
